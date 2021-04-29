9c890465603c6a280d667d6e0307823a7993b6ec
Patrick Coxall <mr.coxall@mths.ca>
2021-04-29 01:34:25 +0000

    lint fix

--- helloWorld.go
+++ helloWorld.go
@@ -1,9 +1,7 @@
-/*
-	* Created by: Mr Coxall
-	* Created on: Sep 2020
-	*
-	* This program displays, "Hello, World!"
-*/
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program displays, "Hello, World!"
 
 package main
 
